f808c84ad398f845db32c7ed29a169025a9cd3b2
Pransh-Coder <rastogipransh4@gmail.com>
2020-02-16 22:33:21 +0530

    Final App

---
+++ app/src/main/java/com/example/embeddedcameraapp/CameraPreview.java
@@ -0,0 +1,106 @@
+package com.example.embeddedcameraapp;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/*  A camera preview class is a SurfaceView that can display the live image data coming from a camera, so users can frame and capture a picture or video.*/
+
+/*preview here means showing images that the camera presently can see on surfaceVew*/
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {    //classPreview class implements SurfaceHolder.Callback in order to capture the callback events for creating and destroying the view, which are needed for assigning the camera preview input.
+
+    private SurfaceHolder mHolder;
+    private Camera mCamera;
+
+
+    //constructor
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        mCamera = camera;
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        mHolder = getHolder();
+        mHolder.addCallback(this);
+
+        // deprecated setting, but required on Android versions prior to 3.0
+        mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+
+
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            Log.d("CameraPreview","surface created!");
+            mCamera.setPreviewDisplay(holder);
+            mCamera.startPreview();
+        } catch (IOException e) {
+            Log.d("surfaceCreated", "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+        if (mHolder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+        // stop preview before making changes
+        try {
+            mCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            mCamera.setDisplayOrientation(0);
+            mCamera.lock();
+        }
+        else {
+            mCamera.setDisplayOrientation(90);
+            mCamera.lock();
+        }
+
+        // start preview with new settings
+        try
+        {
+            /*if(surfaceHolder.getSurface()==null){
+                Log.d("mholder","not null");
+                mCamera.setPreviewDisplay(surfaceHolder);
+                mCamera.stopPreview();
+
+            }
+           *//* mCamera.setPreviewDisplay(mHolder);
+            mCamera.startPreview();*//*
+           else {
+               mCamera.startPreview();
+            }*/
+
+            mCamera.setPreviewDisplay(mHolder);
+            MainActivity.safeToTakePicture = true;
+            mCamera.startPreview();
+
+        } catch (Exception e){
+            Log.d("surfaceChanged", "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+}
---
+++ app/src/main/java/com/example/embeddedcameraapp/MainActivity.java
@@ -0,0 +1,263 @@
+package com.example.embeddedcameraapp;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
+import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO;
+
+public class MainActivity extends Activity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;                 //obj of CameraPreview Class
+    public static String TAG = "MainActivity";
+    public  static boolean safeToTakePicture = false;
+    public boolean pictureTaken=false;
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+    private FrameLayout preview;
+    //FrameLayout preview;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        checkAndRequestPermissions();
+
+        checkCameraHardware(this);
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);  //calling constructor
+        preview =  findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+
+        final Button captureButton = findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(!pictureTaken){
+                            if (mCamera == null) {
+//                            finish();
+                                Toast.makeText(MainActivity.this, "its null", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                mCamera.takePicture(null, null, mPicture);       //In order to retrieve a picture, use the Camera.takePicture() method. This method takes three parameters which receive data from the camera;
+                                captureButton.setText("Retake");
+                                Log.d("log",mPreview.toString());
+                            }
+                        }else{
+                            captureButton.setText("capture");
+                            //Toast.makeText(MainActivity.this, "Refreshed", Toast.LENGTH_SHORT).show();
+//                            preview.removeAllViews();
+                            preview.addView(new CameraPreview(MainActivity.this,mCamera));
+                            pictureTaken=false;
+//                            mCamera.takePicture(null,null,mPicture);
+                        }
+                    }
+                }
+        );
+    }
+
+    /**
+     * Check if this device has a camera
+     */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            //Toast.makeText(context, "Camera not Present!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    /**
+     * A safe way to get an instance of the Camera object.
+     */
+    public static Camera getCameraInstance() {          // return obj of Camera type
+        Camera c = null;
+        try {
+            Log.d("inside try", "try");
+            c = Camera.open(); // attempt to get a Camera instance     for primary camera
+        } catch (Exception e) {
+            // Camera is not available (in use or does not exist)
+            Log.e("getCameraInstance", e.toString());
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {            // In order to receive data in a JPEG format, you must implement an Camera.PictureCallback interface to receive the image data and write it to a file
+
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
+            if (pictureFile == null) {
+                //safeToTakePicture = true;
+                Log.d(TAG, "Error creating media file, check storage permissions");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                //Toast.makeText(MainActivity.this, "Saved!", Toast.LENGTH_SHORT).show();
+                fos.close();
+                pictureTaken=true;
+               /* Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(0, 0);*/
+
+
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "File not found: " + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "Error accessing file: " + e.getMessage());
+            }
+            //safeToTakePicture = true;
+        }
+    };
+
+    /**
+     * Create a file Uri for saving an image or video
+     */
+    private Uri getOutputMediaFileUri(int type) {
+
+        //To show img in gallery
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(Uri.fromFile(getOutputMediaFile(type)));
+        sendBroadcast(intent);      //sending broadcast to gallery
+
+        return Uri.fromFile(getOutputMediaFile(type));
+
+    }
+
+    /**
+     * Create a File for saving an image or video
+     */
+    private static File getOutputMediaFile(int type) {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_" + timeStamp + ".jpg");
+            Log.e("mainActy", mediaStorageDir.getPath());
+        } else if (type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_" + timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //releaseMediaRecorder();       // if you are using MediaRecorder, release it first
+        releaseCamera();              // release the camera immediately on pause event
+
+    }
+
+    @Override
+    protected void onResume() {
+        if(mCamera==null){
+            mCamera=getCameraInstance();
+        }
+        preview.addView(new CameraPreview(MainActivity.this,mCamera));
+        //Toast.makeText(this, "onRseumme called ", Toast.LENGTH_SHORT).show();
+        super.onResume();
+    }
+
+    private void releaseCamera() {
+        if (mCamera != null) {
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
+    }
+
+    private void checkAndRequestPermissions() {
+
+        int camera = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA);       // To check if you have permission call ContextCompat.checkSelfPermission
+        int storage = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        List<String> listPermissionsNeeded = new ArrayList<>();
+
+        if (camera != PackageManager.PERMISSION_GRANTED) {                  // if camera permission is not granted ask permission and add it to arraylist listPermissionsNeeded using add funct
+            listPermissionsNeeded.add(android.Manifest.permission.CAMERA);
+        }
+        if (storage != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty())    // if arraylist listPermissionsNeeded is not empty
+        {
+            ActivityCompat.requestPermissions(this,listPermissionsNeeded.toArray                //requestPermissions- used for requesting the permissions
+                    (new String[listPermissionsNeeded.size()]),REQUEST_ID_MULTIPLE_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==REQUEST_ID_MULTIPLE_PERMISSIONS){
+            Intent intent=new Intent(MainActivity.this,MainActivity.class);
+            startActivity(intent);
+            overridePendingTransition(0,0);
+            finish();
+        }
+    }
+}
+
